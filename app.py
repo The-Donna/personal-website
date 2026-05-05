4d1e1f976026f79d8784930e3484d88d9dc015d5
Gloria Atolagbe <ga466@drexel.edu>
2026-05-05 13:16:29 -0400

    Changed index.html and css styles

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ def contact():
     
     return render_template('contact.html', title="Contact")
 
-# Handles both Render deployment and local development
+# Handles local development
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', 'False') == 'True')
     
